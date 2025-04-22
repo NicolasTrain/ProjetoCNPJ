e160597ea51f819847c58b54dfe7ab6b762374e6
NicolasTrain <nicolas_train@hotmail.com>
2025-04-22 19:44:56 -0300

    Lista finalizada aguardando continuação

--- src/scraping.py
+++ src/scraping.py
@@ -15,8 +15,19 @@ if requests.get(url_mes_atual).status_code == 200:
     ultimo_mes_atulizado = today
     url_ultimo_mes_atualizado = url_mes_atual
 else: 
-    ultimo_mes_atulizado = last_month
+    ultimo_mes_atualizado = last_month
     url_ultimo_mes_atualizado = url_mes_passado
 html_texto = requests.get(url_ultimo_mes_atualizado).content
 tree = html.fromstring(html_texto)
-print(html_texto)
+
+for link in tree.xpath('//a[text()="Parent Directory"]'):
+    link.getparent().remove(link)
+for link in tree.xpath('//a[starts-with(@href, "?")]'):
+    link.getparent().remove(link)
+
+html_limpo = html.tostring(tree, pretty_print=True, encoding='unicode')
+print(html_limpo)
+hrefs = [a.get('href') for a in tree.xpath('//a[@href]')]
+print("\nHREFs encontrados:")
+for href in hrefs:
+    print(href)
